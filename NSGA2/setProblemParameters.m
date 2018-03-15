60f7da8d32ff63445dcd4f014280a461d7bba97e
Romain <romain.vanmarcke@isen.yncrea.fr>
2018-03-14 21:51:34 -0400

    ajout NSGA2

---
+++ NSGA2/setProblemParameters.m
@@ -0,0 +1,54 @@
+function problem = setProblemParameters(problem)
+    switch(func2str(problem.fitnessFunction))
+        case "schaffer"
+            problem.lower = [-1000];
+            problem.upper = [1000];
+            problem.L = 1;
+%             A = 0:0.1:2;
+            A = linspace(0,2,500);
+            problem.pareto = A(:);
+        case "fonseca"
+            problem.lower = [-4,-4,-4];
+            problem.upper = [4,4,4];
+            problem.L = 3;
+%             A = (-1/sqrt(3)):0.1:(1/sqrt(3));
+            A = linspace((-1/sqrt(3)),(1/sqrt(3)),500);
+            problem.pareto = [A(:), A(:), A(:)];
+        case "poloni"
+            problem.lower = [-pi, -pi];
+            problem.upper = [pi, pi];
+            problem.L = 2;
+            problem.pareto = 0;
+        case "kursawe"
+            problem.lower = [-5, -5, -5];
+            problem.upper = [5, 5, 5];
+            problem.L = 3;
+            problem.pareto = 0;
+        case {"zdt1", "zdt2", "zdt3"}
+            problem.lower = zeros(1,30);
+            problem.upper = ones(1,30);
+            problem.L = 30;
+            A = linspace(0,1,500);
+            B = zeros(length(A),problem.L);
+            B(:,1) = A(:);
+            problem.pareto = B;
+        case "zdt4"
+            problem.lower = [0, -5, -5, -5, -5, -5, -5, -5, -5, -5];
+            problem.upper = [1, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+            problem.L = 10;
+            A = linspace(0,1,500);
+            B = zeros(length(A),problem.L);
+            B(:,1) = A(:);
+            problem.pareto = B;
+        case "zdt6" 
+            problem.lower = zeros(1,10);
+            problem.upper = ones(1,10);
+            problem.L = 10;
+            A = linspace(0,1,500);
+            B = zeros(length(A),problem.L);
+            B(:,1) = A(:);
+            problem.pareto = B;
+    end
+    
+end
+
